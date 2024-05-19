b91e32f6b4ed43a947c7467c480db22f6406632c
destroyer <destroyer.dcf@gmail.com>
2024-05-19 11:05:52 +0200

    primera release

--- Formula/Cpcready.rb
+++ Formula/Cpcready.rb
@@ -1,22 +1,51 @@
-class Cpcready < Formula
-    desc "My CPCReady software"
+# class Cpcready < Formula
+#     desc "My CPCReady software"
+#     homepage "https://github.com/CPCReady/sdk"
+#     version "1.0.0"
+#     url "https://github.com/CPCReady/sdk/releases/download/v1.0.1/CPCReady.tar.gz"
+#     sha256 "7e006dd18d7e987a2959f0c21546b6208c312a2f56c20086bfc3ccc3332aa553"
+  
+#     def install
+#       # Crear el directorio /opt/CPCReady
+#       opt_prefix = "/opt/CPCReady"
+#       (opt_prefix).install Dir["*"]
+  
+#       # Crear enlaces simbólicos en /usr/local/bin para cada ejecutable en /opt/CPCReady/bin
+#       bin.install_symlink Dir["#{opt_prefix}/bin/*"]
+#     end
+  
+#     test do
+#       # Comprobar que el software se instaló correctamente y puede ejecutarse
+#       assert_predicate opt_prefix/"bin/about", :exist?
+#       assert_match "expected_output", shell_output("#{bin}/about --version")
+#     end
+#   end
+
+  class CpcReady < Formula
+    desc "CPCReady SDK"
     homepage "https://github.com/CPCReady/sdk"
-    version "1.0.0"
+    version "1.0.1"
     url "https://github.com/CPCReady/sdk/releases/download/v1.0.1/CPCReady.tar.gz"
     sha256 "7e006dd18d7e987a2959f0c21546b6208c312a2f56c20086bfc3ccc3332aa553"
   
     def install
-      # Crear el directorio /opt/CPCReady
-      opt_prefix = "/opt/CPCReady"
-      (opt_prefix).install Dir["*"]
+      # Crear el directorio /opt/CPCReady si no existe
+      opt_prefix = Pathname.new("/opt/CPCReady")
+      opt_prefix.mkpath
+  
+      # Copiar todos los archivos al directorio /opt/CPCReady
+      opt_prefix.install Dir["*"]
   
       # Crear enlaces simbólicos en /usr/local/bin para cada ejecutable en /opt/CPCReady/bin
-      bin.install_symlink Dir["#{opt_prefix}/bin/*"]
+      (opt_prefix/"bin").each_child do |executable|
+        bin.install_symlink executable
+      end
     end
   
     test do
       # Comprobar que el software se instaló correctamente y puede ejecutarse
-      assert_predicate opt_prefix/"bin/about", :exist?
+      assert_predicate "/opt/CPCReady/bin/about", :exist?
       assert_match "expected_output", shell_output("#{bin}/about --version")
     end
   end
+  
