90cd7d67778134a14a7f0be34b6fe7384cc26aac
destroyer <destroyer.dcf@gmail.com>
2024-05-19 12:24:11 +0200

    primer commit

--- Formula/Cpcready.rb
+++ Formula/Cpcready.rb
@@ -1,3 +1,27 @@
+class Cpcready < Formula
+  desc "CPCReady SDK"
+  homepage "https://github.com/CPCReady/sdk"
+  url "https://github.com/CPCReady/sdk/releases/download/v1.0.1/CPCReady.tar.gz"
+  sha256 "9744e959095cb4fb329109eab67000ca73d4040c914017953e73376153fceb1b"
+
+  def install
+    # Instala los archivos binarios en el directorio 'bin'
+    bin.install "cls", "console", "about", "configuration"
+
+    # Instala los archivos de datos en el directorio 'share'
+    share.install "cpc"
+  end
+
+  test do
+    # Verifica que los ejecutables se instalaron correctamente
+    assert_equal "1.0.1", shell_output("#{bin}/about --version").strip
+
+    # Verifica que el archivo 'cls' se puede leer desde el directorio 'share'
+    assert_predicate share/"cpc/cls", :exist?
+  end
+end
+
+
 # class Cpcready < Formula
 #     desc "My CPCReady software"
 #     homepage "https://github.com/CPCReady/sdk"
@@ -21,59 +45,59 @@
 #     end
 #   end
 
-class Cpcready < Formula
-    desc "CPCReady SDK"
-    homepage "https://github.com/CPCReady/sdk"
-    # version "1.0.1"
-    url "https://github.com/CPCReady/sdk/releases/download/v1.0.1/CPCReady.tar.gz"
-    sha256 "9744e959095cb4fb329109eab67000ca73d4040c914017953e73376153fceb1b"
+# class Cpcready < Formula
+#     desc "CPCReady SDK"
+#     homepage "https://github.com/CPCReady/sdk"
+#     # version "1.0.1"
+#     url "https://github.com/CPCReady/sdk/releases/download/v1.0.1/CPCReady.tar.gz"
+#     sha256 "9744e959095cb4fb329109eab67000ca73d4040c914017953e73376153fceb1b"
   
-    def install
-      # Crear el directorio /opt/CPCReady si no existe
+#     def install
+#       # Crear el directorio /opt/CPCReady si no existe
 
-      # bash_shellenv.install <<-SHELL
-      # export SDKMAN_DIR="$HOME/.sdkman"
-      # . "$HOME/.sdkman/bin/sdkman-init.sh"
-      # SHELL
-      # zsh_shellenv.install <<-SHELL
-      #     export SDKMAN_DIR="$HOME/.sdkman"
-      #     . "$HOME/.sdkman/bin/sdkman-init.sh"
-      # SHELL
-      # on_linux do
-      #   depends_on "util-linux"
-      # end
-    #   install_dir = File.expand_path("~/CPCReady")
+#       # bash_shellenv.install <<-SHELL
+#       # export SDKMAN_DIR="$HOME/.sdkman"
+#       # . "$HOME/.sdkman/bin/sdkman-init.sh"
+#       # SHELL
+#       # zsh_shellenv.install <<-SHELL
+#       #     export SDKMAN_DIR="$HOME/.sdkman"
+#       #     . "$HOME/.sdkman/bin/sdkman-init.sh"
+#       # SHELL
+#       # on_linux do
+#       #   depends_on "util-linux"
+#       # end
+#     #   install_dir = File.expand_path("~/CPCReady")
 
-    #   # Crea la carpeta si no existe
-    #   mkdir_p install_dir
+#     #   # Crea la carpeta si no existe
+#     #   mkdir_p install_dir
   
-    #   # Copia todos los archivos a la carpeta de instalación
-    # # Copia todos los archivos a la carpeta de instalación
-    #   cp_r Dir["*"], install_dir
-            # uses_from_macos "jq"
-      # uses_from_macos "dos2unix"
-      # opt_prefix = Pathname.new("$HOME/CPCReady")
-      # opt_prefix.mkpath
-      bin.install "cls"
-      bin.install "console"
-      bin.install "about"
-      bin.install "configuration"
-      share.install "cpc"
-      # # Copiar todos los archivos al directorio /opt/CPCReady
-      # opt_prefix.install Dir["*"]
+#     #   # Copia todos los archivos a la carpeta de instalación
+#     # # Copia todos los archivos a la carpeta de instalación
+#     #   cp_r Dir["*"], install_dir
+#             # uses_from_macos "jq"
+#       # uses_from_macos "dos2unix"
+#       # opt_prefix = Pathname.new("$HOME/CPCReady")
+#       # opt_prefix.mkpath
+#       bin.install "cls"
+#       bin.install "console"
+#       bin.install "about"
+#       bin.install "configuration"
+#       share.install "cpc"
+#       # # Copiar todos los archivos al directorio /opt/CPCReady
+#       # opt_prefix.install Dir["*"]
   
-      # # Crear enlaces simbólicos en /usr/local/bin para cada ejecutable en /opt/CPCReady/bin
-      # (opt_prefix/"bin").each_child do |executable|
-      #   bin.install_symlink executable
-      # end
-      # bin.install_symlink Dir["#{install_dir}/bin/*"]
-    end
+#       # # Crear enlaces simbólicos en /usr/local/bin para cada ejecutable en /opt/CPCReady/bin
+#       # (opt_prefix/"bin").each_child do |executable|
+#       #   bin.install_symlink executable
+#       # end
+#       # bin.install_symlink Dir["#{install_dir}/bin/*"]
+#     end
   
-    test do
-      # Comprobar que el software se instaló correctamente y puede ejecutarse
-      system "#{bin}/about", "--version"
-      # assert_predicate "/opt/CPCReady/bin/about", :exist?
-      # assert_match "expected_output", shell_output("#{bin}/about --version")
-    end
-  end
+#     test do
+#       # Comprobar que el software se instaló correctamente y puede ejecutarse
+#       system "#{bin}/about", "--version"
+#       # assert_predicate "/opt/CPCReady/bin/about", :exist?
+#       # assert_match "expected_output", shell_output("#{bin}/about --version")
+#     end
+#   end
   
