93f02711d298d2b888945592393749063c99a96f
destroyer <destroyer.dcf@gmail.com>
2024-05-26 00:54:40 +0200

    install test

--- Formula/Cpcready.rb
+++ Formula/Cpcready.rb
@@ -20,8 +20,9 @@ class Cpcready < Formula
     if OS.mac?
       # lib.install "libexec/rvm.app"
       # lib.install "libexec/CPCemuMacOS.app"
-      system "mkdir","-p","#{ENV['HOME']}/.CPCReady/"
-      system "cp", "-r", "libexec/rvm.app", "#{ENV['HOME']}/.CPCReady/"
+      app "libexec/rvm.app", target: "cpcready-rvm.app"
+      # system "mkdir","-p","#{ENV['HOME']}/.CPCReady/"
+      # system "cp", "-r", "libexec/rvm.app", "#{ENV['HOME']}/.CPCReady/"
     end
 
     # Instala cpcemu solo en Linux
