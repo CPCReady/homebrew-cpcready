24d8f31ac4991e448a358632e06819269d8c0b0e
destroyer <destroyer.dcf@gmail.com>
2024-06-04 16:34:12 +0200

    arreglo de errores

--- Formula/Cpcready.rb
+++ Formula/Cpcready.rb
@@ -28,10 +28,29 @@ class Cpcready < Formula
     sha256 "aabe560fb5e54f7890c657877c461c4dde473073a7fc06a271a339a5d3167a82" 
   end
 
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/66/10/60dfbae0c000879066656fc15bb8d0d1d4235ead415959defcbeb57ee060/prompt_toolkit-3.0.45.tar.gz"
+    sha256 "07c60ee4ab7b7e90824b61afa840c8f5aad2d46b3e2e10acc33d8ecc94a49089"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/6c/63/53559446a878410fc5a5974feb13d31d78d752eb18aeba59c7fef1af7598/wcwidth-0.2.13.tar.gz"
+    sha256 "72ea0c06399eb286d978fdedb6923a9eb47e1c486ce63e9b4e64fc18303972b5"
+  end
+
+  resource "configparser" do
+    url "https://files.pythonhosted.org/packages/fd/1d/a0f55c373f80437607b898956518443b9edd435b5a226392a9ef11d79fa0/configparser-7.0.0.tar.gz"
+    sha256 "af3c618a67aaaedc4d689fd7317d238f566b9aa03cae50102e92d7f0dfe78ba0"
+  end
+
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/bc/57/e84d88dfe0aec03b7a2d4327012c1627ab5f03652216c63d49846d7a6c58/python-dotenv-1.0.1.tar.gz"
+    sha256 "e324ee90a023d808f1959c46bcbc04446a10ced277783dc6ee09987c37ec10ca"
+  end
+
   depends_on "dos2unix"
   depends_on "jq"
   depends_on "python@3.12"
-  depends_on "python-prompt-toolkit"
 
   def install
     bin.install "bin/about", "bin/cls", "bin/amsdos", "bin/disc", "bin/emulator", "bin/lcat", "bin/mode", "bin/model", "bin/new", "bin/run", "bin/save"
@@ -65,6 +84,10 @@ class Cpcready < Formula
 
     # Instala el módulo Python amsdospy
     venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resource("configparser")
+    venv.pip_install resource("prompt-toolkit")
+    venv.pip_install resource("wcwidth")
+    venv.pip_install resource("python-dotenv")
     venv.pip_install resource("amsdospy")
     bin.install_symlink libexec/"bin/amsdospy"
 
