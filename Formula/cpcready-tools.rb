5aa8be5f48780266297a3ce48f698138dd58da6d
destroyer <destroyer.dcf@gmail.com>
2025-09-28 02:15:26 +0200

    add formula CPCReady Tools

---
+++ Formula/cpcready-tools.rb
@@ -0,0 +1,10 @@
+class CpcReadyTools < Formula
+  desc "Instala un conjunto de herramientas CPCReady y algunas externas"
+  homepage "https://github.com/CPCReady/homebrew-cpcready"
+
+  # Dependencias internas y externas
+  depends_on "idsk"          # tu fórmula
+  depends_on "gum"          # tu fórmula
+  depends_on "yq"          # fórmula oficial de Homebrew
+  depends_on "jq"            # otra fórmula pública
+end
