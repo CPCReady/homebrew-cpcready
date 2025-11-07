476863dcc24ab87c549f0e5c4cf40261c3f283c7
destroyer <destroyer.dcf@gmail.com>
2025-11-07 18:30:43 +0100

    update artifacts from Fri Nov  7 18:30:43 CET 2025 version 0.23

--- Formula/idsk.rb
+++ Formula/idsk.rb
@@ -1,19 +1,21 @@
 class Idsk < Formula
   desc "Amstrad CPC Disk Image Management Tool - Professional CLI utility for DSK files"
   homepage "https://github.com/CPCReady/idsk"
-  url "https://github.com/CPCReady/idsk/archive/refs/tags/v0.22.tar.gz"
-  sha256 "d3672ee33351e62a9b60d9e5e883c76a83ef646227f4958cb9ca8a3479e2e674"
+  url "https://github.com/CPCReady/idsk/archive/refs/tags/v0.23.tar.gz"
+  sha256 "304a45ee7083ea108c10b291cd2b658f8a9d389cec3a702c0ef343c640ede5f5"
   license "MIT"
 
   # Bottles para múltiples plataformas
   bottle do
-    root_url "https://github.com/CPCReady/idsk/releases/download/v0.22"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d2f3fd65a224b60537a7a1868e0c998fde01be95419b8e7e408e15c94708fd19"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d2f3fd65a224b60537a7a1868e0c998fde01be95419b8e7e408e15c94708fd19"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d2f3fd65a224b60537a7a1868e0c998fde01be95419b8e7e408e15c94708fd19"
-    sha256 cellar: :any_skip_relocation, sequoia:       "0869b0b2ac7aa7df27fd640fcafd013fad6aa1285c1853a9db95aaacefc00a13"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0869b0b2ac7aa7df27fd640fcafd013fad6aa1285c1853a9db95aaacefc00a13"
-    sha256 cellar: :any_skip_relocation, ventura:       "0869b0b2ac7aa7df27fd640fcafd013fad6aa1285c1853a9db95aaacefc00a13"
+    root_url "https://github.com/CPCReady/idsk/releases/download/v0.23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4f501ef378078a69f185a6684d7940b575ef0a9f8bdaeaaf3f443708294bb524"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f501ef378078a69f185a6684d7940b575ef0a9f8bdaeaaf3f443708294bb524"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4f501ef378078a69f185a6684d7940b575ef0a9f8bdaeaaf3f443708294bb524"
+    sha256 cellar: :any_skip_relocation, sequoia:       "dc9fb2cb525148215f9627136b5e8fef806303f5f2c589a30c3836e9063fbac6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dc9fb2cb525148215f9627136b5e8fef806303f5f2c589a30c3836e9063fbac6"
+    sha256 cellar: :any_skip_relocation, ventura:       "dc9fb2cb525148215f9627136b5e8fef806303f5f2c589a30c3836e9063fbac6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e72838f9c962bf86137941c4697585008ba7fea0d2e85eb751c3694c1846bf18"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "69813fa8067c4e1ecd0be47507e85c646f76a14d1f533a7ccb76967f8fde0e81"
   end
 
   depends_on "cmake" => :build
