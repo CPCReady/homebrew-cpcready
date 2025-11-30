e52374a85a21f1e892a4e9330bc20d062bda03ac
destroyer <destroyer.dcf@gmail.com>
2025-11-30 11:23:23 +0100

    fix: correct requests version and sha256

--- Formula/cpc.rb
+++ Formula/cpc.rb
@@ -45,8 +45,8 @@ class Cpc < Formula
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.5.tar.gz"
-    sha256 "5e1a7c1e2c6c13f89e6f8b30c73efca73dcf91e95be92a0c2d96e1123df6a1b0"
+    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   def install
